954924e7664fc373f90bfa1d49799570d8b5fec1
Oscared <oornberg@kth.se>
2018-02-20 16:58:43 +0100

    Update

--- ProjectChat/src/ServerThread.java
+++ ProjectChat/src/ServerThread.java
@@ -1,20 +1,47 @@
+import java.io.*;
+import java.net.*;
+
+
 public class ServerThread extends Thread {
 
+    //Socket to connect the client to. Will come from the accepted socket 
+    //to our serverSocket
     private Socket clientSocket;
 
+    //Output from client.
     private PrintWriter writer;
 
+    //Input to the client.
     private BufferedReader reader;
 
-    private Int ID;
+    //Unique ID for the client used for identification.
+    private int ID;
 
-    public ServerThread(Socket clientSocket, int ID) {
+    //Creates a new thread for the new client with a unique ID.
+    public ServerThread(Socket sock, int id) {
+        clientSocket = sock;
+        ID = id; 
     }
 
+    @Override
     public void run() {
+        try{
+	    writer = new PrintWriter(
+				  clientSocket.getOutputStream(), true);
+	}catch(IOException e){
+	    System.out.println("getOutputStream failed: " + e);
+	    System.exit(1);
+	}
+	try{
+	    reader = new BufferedReader(new InputStreamReader(
+	            clientSocket.getInputStream()));
+	}catch(IOException e){
+	    System.out.println("getInputStream failed: " + e);
+	    System.exit(1);
+	}
     }
 
-    public Int GetID() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public int GetID() {
+        return ID;
     }
 }
