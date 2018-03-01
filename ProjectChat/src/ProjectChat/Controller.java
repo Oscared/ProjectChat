e8eae3c23d48b420e8813d70192cee728f3192a6
95mars <Martin.c.e.stahl@gmail.com>
2018-03-01 16:18:40 +0100

    decisionCounter

--- ProjectChat/src/ProjectChat/Controller.java
+++ ProjectChat/src/ProjectChat/Controller.java
@@ -1,44 +1,62 @@
 package ProjectChat;
 
-
 import ProjectChat.Conversation;
-import ProjectChat.PortPanel;
+import ProjectChat.ControllerFrame;
 import java.awt.event.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class Controller implements ActionListener{
+public class Controller implements ActionListener {
 
     private ServerSocket serverSocket;
-   
-    private PortPanel startView;
+
+    private ControllerFrame startView;
+    int decisionCounter;
 
     private List<Conversation> conversationList = new ArrayList<>();
-    
+    int IDCounter = 0;
+
     private String lastText;
 
     public Controller(int port) {
         try {
-            startView = new PortPanel();
+            startView = new ControllerFrame();
             startView.connectButton.addActionListener(this);
             serverSocket = new ServerSocket(port);
 
             Thread connectionThread = new Thread() {
                 public void run() {
-                    int IDCounter = 0;
                     while (true) {
                         Socket newSocket = null;
                         try {
                             newSocket = serverSocket.accept();
                             if (newSocket != null) {
-                                Conversation newConversation = new Conversation();
-                                newConversation.view.connectButton.addActionListener(Controller.this);
                                 ServerThread newThread = new ServerThread(newSocket, IDCounter);
-                                newConversation.add(newThread);
-                                conversationList.add(newConversation);
-                                IDCounter = IDCounter + 1;
+                                PopUpConnect connectRequest = new PopUpConnect();
+                                connectRequest.acceptButton.addActionListener(Controller.this);
+                                connectRequest.declineButton.addActionListener(Controller.this);
+                                decisionCounter = -2;
+                                while (decisionCounter == -2) {
 
+                                }
+                                if (decisionCounter == -1) {
+                                    Conversation newConversation = new Conversation();
+                                    startView.tabbedPane.addTab("Chat" + IDCounter, newConversation.view);
+                                    newConversation.view.disconnectButton.addActionListener(Controller.this);
+                                    newConversation.add(newThread);
+                                    conversationList.add(newConversation);
+                                    decisionCounter = -2;
+                                }
+                                if (decisionCounter >= 0) {
+                                    conversationList.get(decisionCounter).add(newThread);
+                                    decisionCounter = -2;
+
+                                } else {
+                                    newThread.stopThread();
+                                    newSocket.close();
+                                    decisionCounter = -2;
+                                }
                             }
                         } catch (IOException e) {
                             System.out.println(e.getMessage());
@@ -56,20 +74,14 @@ public class Controller implements ActionListener{
         System.out.println("Controller is init");
         Controller newController = new Controller(4444);
         System.out.println("Controller is done");
-        /*try {
-            Socket conSock = new Socket("130.229.143.175.", 4444);
-            Conversation newConversation = new Conversation();
-            ServerThread newThread = new ServerThread(conSock, 1);
-            newConversation.add(newThread);
-        } catch (Exception e) {
-            e.getMessage();
-        }*/
     }
-    public void startNewConv(String iP, int port, String name) {
-            try {
-            Socket conSock = new Socket(iP, port);
 
+    public void startNewConv(String iP, int port, String name) {
+        try {
+            Socket conSock = new Socket(iP, port);
             Conversation newConversation = new Conversation();
+            startView.tabbedPane.addTab("Chat" + IDCounter, newConversation.view);
+            IDCounter = IDCounter + 1;
             ServerThread newThread = new ServerThread(conSock, 1);
             newConversation.add(newThread);
         } catch (Exception e) {
@@ -80,9 +92,18 @@ public class Controller implements ActionListener{
     public String sendMess(String text) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    public void actionPerformed(ActionEvent e){
-        startNewConv(startView.ipField.getText(), 
-                     Integer.parseInt(startView.portField.getText()), 
-                     startView.nameField.getText());
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == "connectButton") {
+            startNewConv(startView.ipField.getText(),
+                    Integer.parseInt(startView.portField.getText()),
+                    startView.nameField.getText());
+        }
+        if (e.getSource() == "acceptButton") {
+            decisionCounter = 1;
+        }
+        if(e.getSource() == "declineButton"){
+            decisionCounter =-99;
+        }
     }
 }
